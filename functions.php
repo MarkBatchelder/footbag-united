193ff2f7f785e282b5e793e519295f4da959beef
WPAddiction <code@wpaddiction.com>
2018-01-10 17:20:58 -0800

    Added fonts, content width, and page breaks for freestyle moves/types custom post types.

--- functions.php
+++ functions.php
@@ -23,4 +23,44 @@ function theme_enqueue_styles() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-?>
+
+/**
+ * Enqueue fonts.
+ *
+ * @link http://www.wpbeginner.com/wp-themes/how-add-google-web-fonts-wordpress-themes/
+ */
+function footbag_fonts() {
+    wp_enqueue_style( 'footbag_fonts', 'https://fonts.googleapis.com/css?family=Acme', false );
+}
+add_action('wp_enqueue_scripts', 'footbag_fonts');
+
+/**
+ * Set up the content width value based on the theme's design.
+ *
+ * @see twentyfourteen_content_width()
+ *
+ * @since Twenty Fourteen 1.0
+ */
+if ( ! isset( $content_width ) ) {
+	$content_width = 750;
+}
+
+/**
+ * @desc Set posts per page for custom post types and taxonomies
+ */
+function twentyfourteen_custom_posts_per_page($query) {
+    if (!$query->is_main_query())
+        return $query;
+    elseif ($query->is_post_type_archive('moves') || $query->is_tax('move_difficulty'))
+        $query->set('posts_per_page', '100');
+    elseif ($query->is_post_type_archive('moves') || $query->is_tax('move_type'))
+        $query->set('posts_per_page', '100');
+        $query->set('orderby', 'title');
+        $query->set('order', 'ASC');
+    return $query;
+}
+ 
+// Apply pre_get_posts filter - ensure this is not called when in admin
+if (!is_admin()) {
+    add_filter('pre_get_posts', 'twentyfourteen_custom_posts_per_page');
+}
