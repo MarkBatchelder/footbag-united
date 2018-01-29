0d87cab22b9933b5c99b55bd78fdebb496691b43
WPAddiction <code@wpaddiction.com>
2018-01-29 00:05:23 -0800

    Changing "comment" to "video example".
    Still need to fix "Leave a reply".

--- comments-moves.php
+++ comments-moves.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying Comments
+ * The template for displaying Move Comments
  *
  * The area of the page that contains comments and the comment form.
  *
@@ -27,13 +27,13 @@ if ( post_password_required() ) {
 			$comments_number = get_comments_number();
 			if ( '1' === $comments_number ) {
 				/* translators: %s: post title */
-				printf( _x( 'One thought on &ldquo;%s&rdquo;', 'comments title', 'twentyfourteen' ), get_the_title() );
+				printf( _x( 'One video example of a &ldquo;%s&rdquo;', 'comments title', 'twentyfourteen' ), get_the_title() );
 			} else {
 				printf(
 					/* translators: 1: number of comments, 2: post title */
 					_nx(
-						'%1$s thought on &ldquo;%2$s&rdquo;',
-						'%1$s thoughts on &ldquo;%2$s&rdquo;',
+						'%1$s video example of a &ldquo;%2$s&rdquo;',
+						'%1$s video examples of a &ldquo;%2$s&rdquo;',
 						$comments_number,
 						'comments title',
 						'twentyfourteen'
@@ -48,8 +48,8 @@ if ( post_password_required() ) {
 	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
 	<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
 		<h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'twentyfourteen' ); ?></h1>
-		<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'twentyfourteen' ) ); ?></div>
-		<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'twentyfourteen' ) ); ?></div>
+		<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Videos', 'twentyfourteen' ) ); ?></div>
+		<div class="nav-next"><?php next_comments_link( __( 'Newer Videos &rarr;', 'twentyfourteen' ) ); ?></div>
 	</nav><!-- #comment-nav-above -->
 	<?php endif; // Check for comment navigation. ?>
 
@@ -66,13 +66,13 @@ if ( post_password_required() ) {
 	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
 	<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
 		<h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'twentyfourteen' ); ?></h1>
-		<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'twentyfourteen' ) ); ?></div>
-		<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'twentyfourteen' ) ); ?></div>
+		<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Videos', 'twentyfourteen' ) ); ?></div>
+		<div class="nav-next"><?php next_comments_link( __( 'Newer Videos &rarr;', 'twentyfourteen' ) ); ?></div>
 	</nav><!-- #comment-nav-below -->
 	<?php endif; // Check for comment navigation. ?>
 
 	<?php if ( ! comments_open() ) : ?>
-	<p class="no-comments"><?php _e( 'Comments are closed.', 'twentyfourteen' ); ?></p>
+	<p class="no-comments"><?php _e( 'Video examples are closed.', 'twentyfourteen' ); ?></p>
 	<?php endif; ?>
 
 	<?php endif; // have_comments() ?>
